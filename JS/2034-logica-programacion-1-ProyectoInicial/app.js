e65999d85b3257c2f9c16e7cee8a676e3c87484c
arosaspa <arosaspa@engineer.com>
2025-01-23 15:22:19 -0500

    update

--- JS/2034-logica-programacion-1-ProyectoInicial/app.js
+++ JS/2034-logica-programacion-1-ProyectoInicial/app.js
@@ -1,20 +1,46 @@
-alert("Game of Numbers");
+//Game of Numbers
 
-let numSecret = 4;
-let numUser = prompt("Give me a number please!: ");
+    alert("***Game of Numbers***");
 
-// alert("Play of Numbers from JS ");
+    //variables 
+    let numSecret = 5;
+    let numUser = 0;
+    let attempts = 1;
+   // let word = 'attempt';
+    let attemptMax = 3;
 
-console.log(numUser);
+    // process
+    while (numUser != numSecret) {
+    numUser = prompt("Give me a number of 1 to 10, please!: ");
+     
 
-if (numUser == numSecret)
-{
-    alert("The number is correct: " + numUser); 
-} else {
-    alert("Sorry!, Not's the number");
-}
+    console.log(numUser);
 
+    if (numUser == numSecret)
+    {
+        // alert("The number is correct: " + numUser); 
+        alert(`Congratulations!\nThe number correct is: ${numSecret} \nYou do in ${attempts}  ${attempts == 1 ? `* Attempt *` : ` AtEmPtS`} `);
+    } else {
+        if (numUser > numSecret){
+            alert('The secret number is minor');
+        }  else {
+            alert('The secret number is greater');
+        }
+
+        // increment attempts 
+        /* attempts = attempts + 1;
+        word  =  'attempt'; */
+        attempts ++;
+
+        if (attempts > attemptMax) {
+            alert(`****GAME OVER!*** \n*****Maxium of attempts!***** \n The Attempts was of: ${attemptMax}`);
+            break;
+        }
+        
+        //alert("Sorry!, Not's the number, try again");
+    }
+    }
 
 /*
     prompt("***The number not's correct X***" + numUser);
-} */
+ */
---
+++ JS/2034-logica-programacion-1-ProyectoInicial/test_2.js
@@ -0,0 +1,13 @@
+// **** TEST 2 ****
+
+alert("Count of 10 numbers");
+
+let real = 1;
+
+while (real <= 10) {
+    console.log(real); 
+    
+    alert("The number are: " + real);
+    real++;
+}
+
